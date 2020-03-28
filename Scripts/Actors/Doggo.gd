80163e82475dda113c567eee161c89797e5a7a76
Domi6242 <dominikfoschini@gmail.com>
2020-03-28 21:34:47 +0100

    added FSM, adapted player, working on doggo

--- Scripts/Actors/Doggo.gd
+++ Scripts/Actors/Doggo.gd
@@ -3,14 +3,50 @@ extends Actor
 onready var nav2d : Navigation2D = $"/root/World/NavMap"
 onready var player : KinematicBody2D = $"/root/World/Player"
 
-export var startFollowDist = 200
+export var startFollowDist = 600
 export var stopFollowDist = 100
 
-func _physics_process(delta):
+func _ready():
+	addState("idle")
+	addState("following")
+	addState("wandering")
+	addState("movingAway")
+	call_deferred("setState", states.following)
+
+func stateLogic(delta):
+	print(state)
 	motionAxis = Vector2.ZERO
-	var distance := global_position.distance_to(player.global_position)
-	if distance > startFollowDist:
-		var path = nav2d.get_simple_path(global_position, player.global_position)
-		
-		motionAxis = path[1] - global_position
-		motionAxis = motionAxis.normalized()
+	if state == states.following:
+		follow(delta)
+
+func getTransition(delta):
+	match state:
+		states.idle:
+			if shouldFollow():
+				return states.following
+		states.following:
+			if shouldStopFollowing():
+				return states.idle
+	return null
+
+func enterState(new, old):
+	pass
+
+func exitState(old, new):
+	pass
+
+func follow(delta):
+	var path = nav2d.get_simple_path(global_position, player.global_position)	
+	motionAxis = path[1] - global_position
+	motionAxis = motionAxis.normalized()
+	
+	move(delta)
+
+func shouldFollow():
+	return global_position.distance_to(player.global_position) > startFollowDist
+
+func shouldStopFollowing():
+	return global_position.distance_to(player.global_position) < stopFollowDist
+
+func shouldMoveAway():
+	return global_position.distance_to(player.global_position) <= stopFollowDist
--- Scripts/Actors/Player.gd
+++ Scripts/Actors/Player.gd
@@ -1,12 +1,34 @@
 extends Actor
-
-func _physics_process(delta):
-	motionAxis = getInputAxis()
-
 	
-func getInputAxis():
-	var axis := Vector2.ZERO
-	axis.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	axis.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	return axis.normalized()
+func _ready():
+	addState("idle")
+	addState("move")
+	addState("attack")
+	call_deferred("setState", states.idle)
 
+func stateLogic(delta):
+	motionAxis = getInputAxis()
+	move(delta)
+
+func getTransition(delta):
+	match state:
+		states.idle:
+			if motionAxis != Vector2.ZERO:
+				return states.move
+		states.move:
+			if motionAxis == Vector2.ZERO:
+				return states.idle
+	return null
+
+func enterState(new, old):
+	pass
+	
+func exitState(old, new):
+	pass
+
+func getInputAxis():
+	if [states.idle, states.move].has(state):
+		return Vector2(
+			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+			Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+		)
