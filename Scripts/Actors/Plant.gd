d89393fbe6ba080dc20caf3eb523c1ffc689e0d9
Sharjeel Qaiser <sq18335@essex.ac.uk>
2020-03-29 08:10:16 +0100

    Added plough reach limit

--- Scripts/Actors/Plant.gd
+++ Scripts/Actors/Plant.gd
@@ -3,7 +3,8 @@ extends Sprite
 onready var tilemap:TileMap = $"/root/World/NavMap/TileMap"
 onready var player:Actor = self.get_parent();
 
-var map_pos:Vector2
+var mouse_pos:Vector2
+var player_pos:Vector2
 enum TileType {WALL=0, GRASS=1, DIRT=2}
 
 #***CREATE EVENT***
@@ -14,12 +15,21 @@ func _ready():
 #***UPDATE EVENT***
 func _process(delta):
 	#TEMP (REFACTOR LATER)
-	_beginTrackingMapPos()
-	if(Input.is_key_pressed(KEY_SPACE) and tilemap.get_cellv(map_pos)==TileType.GRASS):
-		tilemap.set_cellv(map_pos,TileType.DIRT);
-
-
-#Update cartesian coords into map units on mouse position
-func _beginTrackingMapPos():
-	map_pos = tilemap.world_to_map(Vector2(get_global_mouse_position().x,get_global_mouse_position().y))
+	updateMousePos()
+	updatePlayerPos()
 	
+	#Plough the tile on click
+	if(Input.is_mouse_button_pressed(BUTTON_LEFT) and tilemap.get_cellv(mouse_pos)==TileType.GRASS):
+		#Limit the reach (2 tiles in each direction)
+		if(mouse_pos.x<player_pos.x+2 and mouse_pos.x>player_pos.x-2 and mouse_pos.y>player_pos.y-2 and mouse_pos.y<player_pos.y+2):
+			tilemap.set_cellv(mouse_pos,TileType.DIRT)
+		
+
+#Mouse cartesian coords to world units
+func updateMousePos():
+	mouse_pos = tilemap.world_to_map(Vector2(get_global_mouse_position().x,get_global_mouse_position().y))
+
+
+#Player cartesian coords to world units
+func updatePlayerPos():
+	player_pos = tilemap.world_to_map(Vector2(player.position.x,player.position.y))
