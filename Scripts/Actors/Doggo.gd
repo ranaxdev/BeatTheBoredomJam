7e77d76389e57656ad3f69d37d8b0b16c09ec38c
unknown <dominikfoschini@gmail.com>
2020-04-08 15:44:03 +0200

    rmeoved useless code from doggo

--- Scripts/Actors/Doggo.gd
+++ Scripts/Actors/Doggo.gd
@@ -9,32 +9,13 @@ const stopFollowDist := 150
 const startMoveAwayDist := 100
 const stopMoveAwayDist := 150
 
-# ~~~ WANDER STUFF ~~~ #
-const wanderWaitTime := 3.0
-var wanderTimer : Timer
-var shouldWander := false
-
-const circleDistance := 60.0
-const circleRadius := 20.0
-const angleChange := 0.3
-var wanderAngle := 0.0
-var prevSpeed
-const wanderSpeed := 200.0
-
 # ~~~ INIT STATES ~~~ #
 func _ready():
 	randomize()
 	addState("idle")
 	addState("following")
-	addState("wandering")
 	addState("movingAway")
 	call_deferred("setState", states.idle)
-	
-	wanderTimer = Timer.new()
-	wanderTimer.set_one_shot(true)
-	wanderTimer.set_wait_time(wanderWaitTime)
-	wanderTimer.connect("timeout",self,"_onWanderTimerComplete")
-	add_child(wanderTimer)
 
 func stateLogic(delta):
 	for key in states:
@@ -46,8 +27,6 @@ func stateLogic(delta):
 		follow(delta)
 	if state == states.movingAway:
 		moveAway(delta)
-	if state == states.wandering:
-		wander(delta)
 
 
 func getTransition(delta):
@@ -57,17 +36,9 @@ func getTransition(delta):
 				return states.following
 			elif shouldMoveAway():
 				return states.movingAway
-			elif shouldWander:
-				pass
-				#return states.wandering
 		states.following:
 			if shouldStopFollowing():
 				return states.idle
-		states.wandering:
-			if shouldFollow():
-				return states.following
-			elif shouldMoveAway():
-				return states.movingAway
 		states.movingAway:
 			if shouldStopMoveAway():
 				return states.idle
@@ -76,12 +47,8 @@ func getTransition(delta):
 func enterState(new, old):
 	match new:
 		states.idle:
-			wanderTimer.start()
-		states.following:
 			pass
-		states.wandering:
-			prevSpeed = MAX_SPEED
-			MAX_SPEED = wanderSpeed
+		states.following:
 			pass
 		states.movingAway:
 			pass
@@ -92,9 +59,6 @@ func exitState(old, new):
 			pass
 		states.following:
 			pass
-		states.wandering:
-			MAX_SPEED = prevSpeed
-			shouldWander = false
 		states.movingAway:
 			pass
 
@@ -114,17 +78,6 @@ func moveAway(delta):
 		motionAxis = -motionAxis.normalized()
 	move(delta)
 
-func wander(delta):
-	var circleCenter := motion * circleDistance
-	var displacement := circleCenter
-	
-	var _len = displacement.length()
-	displacement.x = cos(wanderAngle) * _len
-	displacement.y = sin(wanderAngle) * _len
-	wanderAngle += (randf() * angleChange) - (angleChange * 0.5)
-	motionAxis = (circleCenter + displacement) - global_position
-	move(delta)
-
 func shouldFollow() -> bool:
 	return global_position.distance_to(player.global_position) > startFollowDist
 
@@ -136,7 +89,4 @@ func shouldMoveAway() -> bool:
 
 func shouldStopMoveAway() -> bool:
 	return global_position.distance_to(player.global_position) > stopMoveAwayDist
-
-func _onWanderTimerComplete():
-	shouldWander = true
 	
