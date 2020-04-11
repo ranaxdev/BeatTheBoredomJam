ae6b41630cd68544703c1ce4b4c69ffead1d6ed4
Domi6242 <dominikfoschini@gmail.com>
2020-04-11 09:10:04 +0200

    spawn points fuck ye

--- Scripts/EnemySpawnPoint.gd
+++ Scripts/EnemySpawnPoint.gd
@@ -4,57 +4,44 @@ export var spawnTimer : int = 5
 export var minAmount : int = 2
 export var maxAmount : int = 5
 
-onready var spawnCircle = $SpawnArea/CollisionShape2D
-onready var timer = $SpawnDelay
-onready var rootNode = get_node("/root/World")
-onready var Enemy = preload("res://Scenes/Actors/Enemy.tscn")
+onready var timer := $SpawnDelay
+onready var rootNode := get_node("/root/World")
+onready var spawnPoints := $SpawnPoints
+onready var enemy_res := preload("res://Scenes/Actors/Enemy.tscn")
 
 var canSpawn := true
+var spawnNow := false
 
 func _ready():
 	randomize()
 
-#func _input(event):
-#	var pressed = event.is_pressed() && !event.is_echo()
-#	if Input.is_key_pressed(KEY_SPACE) && pressed:
-#		spawn()
-
-# returns random point inside circle in global coordinates
-func getRandPosiiton() -> Vector2:
-	var a = randf() * 2 * PI
-	var r = spawnCircle.shape.radius * sqrt(randf())
-	
-	return Vector2(r * cos(a), r * sin(a)) + spawnCircle.global_position
-	
-func generateEnemy():
-	var pos = getRandPosiiton()
-	var new_enemy = Enemy.instance()
-	new_enemy.global_position = pos
-	return new_enemy
+func _physics_process(_delta):
+	if canSpawn && spawnNow:
+		spawn()
 
 func spawn():
+	canSpawn = false
+	spawnNow = false
 	var amount := randi() % (maxAmount - minAmount + 1) + minAmount
-	var attempts := 0
-	var done := false
-	var pos := getRandPosiiton()
-	var spaceState = get_world_2d().direct_space_state
-	
+	var points := spawnPoints.get_children()
 	for i in amount:
-		attempts = 0
-		while attempts < 10:
-			var new_enemy = generateEnemy()
-			var query = Physics2DShapeQueryParameters.new()
-			query.set_shape(new_enemy.get_node("Collision").shape)
-			if spaceState.intersect_shape(query).size() == 0:
-				rootNode.add_child(new_enemy)
-				attempts = 10
-			attempts += 1
+		if points.size() == 0:
+			break
+		var point = randi() % points.size()
+		var pos = points[point]
+		#add enemy to root
+		var enemy = enemy_res.instance()
+		enemy.global_position = pos.global_position
+		rootNode.add_child(enemy)
+		points.remove(point)
+	
 
 func _on_Area2D_body_entered(body):
 	if canSpawn:
-		canSpawn = false
-		timer.start()
-		spawn()
+		spawnNow = true
+
+func _on_TriggerArea_body_exited(body):
+	timer.start()
 
 func _on_SpawnDelay_timeout():
 	canSpawn = true
